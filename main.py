a165930cb730daab0dd32f7399461144bbc0e95b
unknown <bexruzraxmanov59@gmail.com>
2024-06-02 20:32:41 +0500

    gotovo

--- main.py
+++ main.py
@@ -52,18 +52,39 @@
 # else:
 #     print("Idi doleko")
 #11
-num1 = int(input("Number1 : "))
-num2 = int(input("Number2 : "))
-dey = input("deystvie : ")
-if num2 == 0 and dey == "/":
-    print("На 0 делить нельзя")
-elif dey == "-":
-    print(num1 - num2)
-elif dey == "+":
-    print(num1 + num2)
-elif dey == "*":
-    print(num1 * num2)
-elif dey == "/":
-    print(num1 / num2)
-else:
-    print("Нет такого действия")
+# num1 = int(input("Number1 : "))
+# num2 = int(input("Number2 : "))
+# dey = input("deystvie : ")
+# if num2 == 0 and dey == "/":
+#     print("На 0 делить нельзя")
+# elif dey == "-":
+#     print(num1 - num2)
+# elif dey == "+":
+#     print(num1 + num2)
+# elif dey == "*":
+#     print(num1 * num2)
+# elif dey == "/":
+#     print(num1 / num2)
+# else:
+#     print("Нет такого действия")
+# 12
+# открыть Visual Studio Code создать папку внутри папки создать файл name.py открыть его и написать print("Hello,World!") 
+# print("Hello World")
+#13
+#Переменная это коробка в которой мы можем хранить (без ничего другого) один тип данных потом что бы болле удобно его вызвать с помощью print() и т.д.
+#15
+# a = 2132
+# if a % 2 == 0:
+#     print("четное")
+# else:
+#     print("нечетное")
+#16
+#цыкл это функции for и while они работаю без конца при определенных условиях и остнавливаються так же при выполнения действия которое вы задали
+#17
+#что бы создать список в питоне с переменной напишите имя переменной и дайте ей лист,массив [] там можно писать функции такие как remove(),pop(),update() и т.д.
+#18
+#функции это один из основных инструментов для создания удобного и структурированного кода
+#19
+# Задается в начале кода try: и в конце except: если условие внутри try: вызывает ошибку то начинает работать except:
+#20
+## открыть Visual Studio Code создать папку внутри папки создать файл name.py открыть его и написать print("Hello,World!") 
