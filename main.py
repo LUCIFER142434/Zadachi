f58692403236329f565bc591a0416281ab2b8c35
unknown <bexruzraxmanov59@gmail.com>
2024-06-02 19:42:43 +0500

    eight

--- main.py
+++ main.py
@@ -36,3 +36,11 @@
 # num2 = int(num2)
 # num3 = int(num3)
 # print(num1 * num2 * num3)
+#8
+# num1 = int(input("Number1 : "))
+# num2 = int(input("Number2 : "))
+# num3 = int(input("Number3 : "))
+# print(num1)
+# print(num2)
+# print(num1)
+# print(num1+num2+num3)
